2f3e8d1fa9947a9fd94799f88d4ff96de0965287
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2021-01-31 22:50:08 +0100

    created index.html file

--- 01_Day01_Introduction/variable.js
+++ 01_Day01_Introduction/variable.js
@@ -1,6 +1,35 @@
+//question 4
 let string = 'I am Bisola';
 let boolean = true;
 let schoolNames;
 let empty = null;
 
 
+//question 6
+let var1;
+let var2;
+let var3;
+let var4;
+
+//question 7
+let item1 = 'icecream';
+let item2 = 'cake';
+let item3 = 'pie';
+let item4 = 'toppings';
+
+//question 8
+let firstName = 'Bisola';
+let lastName = 'Onaolapo';
+let maritalStatus = 'single';
+let country = 'Nigeria';
+let age = '22';
+
+//question 9
+let userData = 'Bisola, Onaolapo, single, Nigeria, 22';
+
+//question 10
+let myAge = 22;
+let yourAge = 28;
+
+console.log(`I am ${myAge} years old`);
+console.log(`You are ${yourAge} years old`);
