db4b02909adc1cb2d6cedfbabd26787319c1f085
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2021-02-10 16:23:47 +0100

    level 2 questions answered

--- 02_Day_Data_types/main.js
+++ 02_Day_Data_types/main.js
@@ -143,4 +143,17 @@ console.log(getRandomInt(50, 100));
 console.log(getRandomInt(0, 225));
 
 //Question 10
+let js = 'JavaScript';
+let randomIndex = Math.floor(Math.random() * (js.length - 1));
+console.log(js[randomIndex]);
 
+//Question 11
+console.log('1\t1\t1\t1\t1');
+console.log('2\t1\t2\t4\t8');
+console.log('3\t1\t3\t9\t27');
+console.log('4\t1\t4\t16\t64');
+console.log('5\t1\t5\t25\t125');
+
+//Question 12
+let cutOut= sentence.substr(30, 24);
+console.log(cutOut);
