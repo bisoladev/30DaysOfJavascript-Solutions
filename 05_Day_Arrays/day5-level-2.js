10b71a0403031ae254cc558cd2aaabcf82f25d3b
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2021-02-23 23:31:29 +0100

    modified HTML files

--- 05_Day_Arrays/day5-level-2.js
+++ 05_Day_Arrays/day5-level-2.js
@@ -1,4 +1,4 @@
-//Exercise 1
+//Exercise 2
 let text = 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.';
 const fullStop = /\./g;
 const comma = /,/g;
@@ -9,4 +9,13 @@ console.log(removedSymbols);
 let words = removedSymbols.match(/\w+/g);
 console.log(words);
 console.log(words.length);
+
+//Exercise 3
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+shoppingCart.unshift('Meat');
+console.log(shoppingCart);
+/*add 'Meat' in the beginning of your shopping cart if it has not been already added
+- add Sugar at the end of you shopping cart if it has not been already added
+- remove 'Honey' if you are allergic to honey
+- modify Tea to 'Green Tea'*/
 
