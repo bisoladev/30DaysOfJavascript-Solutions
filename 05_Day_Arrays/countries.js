56a606849563d2dbe7a7613bff05b9d4dce078c8
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2021-02-24 15:17:47 +0100

    created main.html file

--- 05_Day_Arrays/countries.js
+++ 05_Day_Arrays/countries.js
@@ -20,4 +20,22 @@ const countries = [
     countries.push('ethiopia');
   }
 
+  //Exercise 1 - Level 3
+  const newCountries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ];
+
+  newCountries.slice(0,11);
+  console.log(newCountries);
+
  
