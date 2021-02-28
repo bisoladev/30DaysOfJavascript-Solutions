ce695ffcb7a3496b6415202490ce5143c84634d1
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2021-02-28 22:01:48 +0100

    Exercise 2 completed

--- 06_Loops/day6-level-1.js
+++ 06_Loops/day6-level-1.js
@@ -17,3 +17,22 @@ do {
   console.log(x)
   x++
 } while (x <= 10)
+
+//Exercise 2
+for (i = 10; i >= 0; i--) {
+    console.log(i);
+}
+
+//while loop
+let w = 10
+while (w >= 0 ) {
+  console.log(w)
+  w--
+}
+
+//do - while loop
+let y = 10
+do {
+  console.log(y)
+  y--
+} while (y >= 0)
