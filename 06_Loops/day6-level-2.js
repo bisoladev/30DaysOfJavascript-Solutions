ca65441b597e01c3b7d666f0c80d7a92039bc0a3
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2021-03-08 10:01:01 +0100

    Exercise 2 completed

--- 06_Loops/day6-level-2.js
+++ 06_Loops/day6-level-2.js
@@ -1,9 +1,17 @@
 //Exercise 1
-let randChars = '';
+/*let randChars = '';
 for(let i = 0; i < 2; i++) {
  let random = Math.random().toString(36).substr(2, 5);
  randChars += random;
 }
 console.log(randChars);
+*/
 
-//
+//Exercise 2
+let random = Math.random().toString(16).substr(2,7);
+for(let i = 0; i < 1; i++) {
+    let hexChars = '#';
+    hexChars += random;
+    console.log(hexChars);
+}
+
