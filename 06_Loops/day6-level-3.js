b0bdf7b1a3b967eeb14a0a1e3c08b672285733e4
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2021-03-14 23:09:46 +0100

    Exercise 2 completed

--- 06_Loops/day6-level-3.js
+++ 06_Loops/day6-level-3.js
@@ -1,8 +1,13 @@
 //Exercise 1
 //Copy countries array(Avoid mutation)
-/*countriesCopy = [];
+
+countriesCopy = [];
 for(country of countries) {
     countriesCopy.push(country);
 }
 console.log(countriesCopy);
-*/
+
+//Exercise 2
+//Copy Countries array, Sort and store in sortedCountries variable
+let sortedCountries = countriesCopy.sort();
+console.log(sortedCountries);
