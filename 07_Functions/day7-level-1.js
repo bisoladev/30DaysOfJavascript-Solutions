ab69b4ec6ca9462d01996671c357822653bd5a6f
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2021-03-16 07:53:26 +0100

    exercise 1 completed

---
+++ 07_Functions/day7-level-1.js
@@ -0,0 +1,9 @@
+//Exercise 1
+//Declare a function _fullName_ and it print out your full name.
+
+function fullName(fName, lName) {
+    const printFullName = `Your fullname is ${fName} ${lName}.`;
+    return printFullName;
+}
+
+console.log(fullName('Bisola', 'Onaolapo'));
