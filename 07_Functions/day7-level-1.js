f9c7e1c01c06649e1ab29d766faf46f005cd82f5
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2021-03-16 10:43:58 +0100

    Exercise 7 completed

--- 07_Functions/day7-level-1.js
+++ 07_Functions/day7-level-1.js
@@ -26,7 +26,7 @@ function addNumbers(num1, num2) {
     let sum = num1 + num2;
     return sum;
 }
-console.log(addNumbers(2,8));
+console.log((`(The sum of two numbers are ${addNumbers(2,8)}`));
 
 //Exercise 4 
 /*An area of a rectangle is calculated as follows: _area = length x width_.
@@ -36,7 +36,7 @@ function areaOfRectangle(length, width) {
     let calcAreaOfRectangle = length * width;
     return calcAreaOfRectangle;
 }
-console.log(areaOfRectangle(5, 10));
+console.log((`The area of rectangle is ${areaOfRectangle(5, 10)}`));
 
 //Exercise 5
 /*A perimeter of a rectangle is calculated as follows: _perimeter= 2x(length + width)_. 
@@ -47,6 +47,37 @@ function perimeterOfRectangle(length, width) {
     return calcPeriOfRectangle;
 }
 
-console.log(perimeterOfRectangle(2,4));
+console.log((`The perimeter of rectangle ${perimeterOfRectangle(2,4)}`));
 
 //Exercise 6
+/* A volume of a rectangular prism is calculated as follows: _volume = length x width x height_.
+Write a function which calculates _volumeOfRectPrism_. */
+
+function volumeOfPrism(length, width, height) {
+    let calcVolumeOfPrism = length * width * height;
+    return calcVolumeOfPrism;
+}
+
+console.log((`The volume of prism is ${volumeOfPrism(2, 3 ,4)}`));
+
+//Exercise 7
+/*Area of a circle is calculated as follows: _area = π x r x r_. 
+Write a function which calculates _areaOfCircle_ */
+
+function areaOfCircle(r, pi = 3.14) {
+    let calcAreaOfCircle = Math.round(pi * r * r);
+    return calcAreaOfCircle;
+}
+
+console.log(((`The area of circle is ${areaOfCircle(3)}`)));
+
+//Exercise 8 
+/* Circumference of a circle is calculated as follows: _circumference = 2πr_. 
+Write a function which calculates _circumOfCircle_ */
+
+function circumferenceOfCircle(r, pi = 3.14) {
+    let calcCirOfCircle = Math.round(pi * r * r);
+    return calcCirOfCircle;
+}
+
+console.log(((`The circumference of circle is ${circumferenceOfCircle(3)}`)));
