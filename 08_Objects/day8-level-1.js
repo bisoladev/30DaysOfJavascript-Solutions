1f5d583f5fb8d58b98e04c5f4ff61b53fe9d50f9
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2021-04-24 09:04:50 +0100

    Exercise 3 completed

--- 08_Objects/day8-level-1.js
+++ 08_Objects/day8-level-1.js
@@ -2,3 +2,21 @@
 // Create an empty object called dog
 
 const dog = {};
+
+//Exercise 2
+//Print the the dog object on the console
+
+console.log(dog);
+
+//Exercise 3
+//Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return _woof woof_
+
+dog.name = 'Furry';
+dog.legs = 4;
+dog.color = 'white';
+dog.age = 3;
+dog.bark = function() {
+  return 'woof woof!';
+}
+
+
