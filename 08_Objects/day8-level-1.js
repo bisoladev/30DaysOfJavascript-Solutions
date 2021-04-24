32ff160c5995af5938e913e60658470c8f0cbc5c
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2021-04-24 10:05:49 +0100

    Exercise 5 completed

--- 08_Objects/day8-level-1.js
+++ 08_Objects/day8-level-1.js
@@ -1,28 +1,44 @@
 //Exercise 1
 // Create an empty object called dog
 
-const dog = {};
+
 
 //Exercise 2
 //Print the the dog object on the console
 
-console.log(dog);
+
 
 //Exercise 3
 //Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return _woof woof_
-
-dog.name = 'Furry';
-dog.legs = 4;
-dog.color = 'white';
-dog.age = 3;
-dog.bark = function() {
-  return 'woof woof!';
+const dog = {
+  name: 'Nala',
+  legs: 4,
+  color: 'white',
+  age: 3,
+  sound: 'woof woof!',
+  bark: function() {
+    let nalaBark = `${this.sound}`
+    return nalaBark;
+  }
 }
 
-console.log(dog.bark);
+console.log(dog.bark());
 
 //Exercise 4
 // Get name, legs, color, age and bark value from the dog object
 
 const dogValues = Object.values(dog);
 console.log(dogValues);
+
+//Exercise 5
+// Set new properties in the dog object: breed, getDogInfo
+
+dog.breed = 'Maltese dog'
+dog.getDogInfo = function () {
+  let bark = this.bark();
+  let myDogInfo = `The name of my dog is ${this.name}, she is ${this.color} in color and has ${this.legs} legs, when she sees me, she barks ${bark}, she is ${this.age} years old`;
+  return myDogInfo;
+}
+
+console.log(dog);
+console.log(dog.getDogInfo());
