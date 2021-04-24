470a50cc91b1d3ac971ca45860e52c408e82e0da
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2021-04-24 13:58:53 +0100

    Exercise 3 completed

--- 08_Objects/day8-level-2.js
+++ 08_Objects/day8-level-2.js
@@ -55,39 +55,70 @@ const users = {
 //Exercise 1
 //Find the person who has many skills in the users object.
 
-let maxi = 0;
-let mostSkilledPerson;
+// let maxi = 0;
+// let mostSkilledPerson;
+
+// for (const entry of Object.entries(users)) {
+//   if (entry[1].skills.length > maxi) {
+//     maxi = entry[1].skills.length;
+//     mostSkilledPerson = entry[0];
+//   }
+// }
+// console.log(mostSkilledPerson);
+
+// //Alternative solution
+// let max = 0;
+// let skilledPerson;
+
+// for(const user of Object.keys(users)) {
+//    if(users[user].skills.length > max) {
+//       max = users[user].skills.length;
+//       skilledPerson = Object.assign({}, users[user]);
+//    }
+// }
+// console.log(skilledPerson.email, max); 
+
+// //Alternative solution
+// let winner;
+// let highest = 0;
+
+// for (const property in users) {
+//     if (users[property].skills.length > highest) {
+//         highest = users[property].skills.length;
+//         winner = property
+//     }
+    
+// }
+// console.log(winner);
+
+//Exercise 2
+// Count logged in users,count users having greater than or equal to 50 points from the following object.
+
+let loggedInUsers = 0;
+usersGreater50Points = 0;
 
 for (const entry of Object.entries(users)) {
-  if (entry[1].skills.length > maxi) {
-    maxi = entry[1].skills.length;
-    mostSkilledPerson = entry[0];
+  if (entry[1].isLoggedIn == true) {
+    loggedInUsers = loggedInUsers + 1;
+  }
+
+  if(entry[1].points >= 50) {
+    usersGreater50Points = usersGreater50Points + 1;
   }
 }
-console.log(mostSkilledPerson);
 
-//Alternative solution
-let max = 0;
-let skilledPerson;
+console.log(loggedInUsers);
+console.log(usersGreater50Points);
 
-for(const user of Object.keys(users)) {
-   if(users[user].skills.length > max) {
-      max = users[user].skills.length;
-      skilledPerson = Object.assign({}, users[user]);
-   }
-}
-console.log(skilledPerson.email, max); 
+//Exercise 3
+// Find people who are MERN stack developer from the users object.
 
-//Alternative solution
-let winner;
-let highest = 0;
-
-for (const property in users) {
-    if (users[property].skills.length > highest) {
-        highest = users[property].skills.length;
-        winner = property
+  let mernStackDevelopers = [];
+  for (const entry of Object.entries(users)) {
+    if (entry[1].skills.includes('MongoDB', 'Express', 'React', 'Node')) {
+      mernStackDevelopers.push(entry[0]);
     }
-    
-}
-console.log(winner);
 
+  }
+
+  console.log(mernStackDevelopers);
