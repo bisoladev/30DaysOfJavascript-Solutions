32f65b510258493ae3ae204115243a2e655720c5
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2021-04-27 08:19:09 +0100

    Exercise 3 completed

---
+++ 09-Higher-order-functions/Day9-level-1/question_1-10.js
@@ -0,0 +1,35 @@
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const products = [
+  { product: 'banana', price: 3 },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: 8 },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
+
+//Exercise 1
+// Explain the difference between **_forEach, map, filter, and reduce_**.
+
+//Exercise 2
+// Define a call function before you use them in forEach, map, filter or reduce.
+
+//Exercise 3
+// Use **_forEach to console.log each country in the countries array.
+
+countries.forEach((element) => console.log(element));
+
+//Exercise 4
+// Use **_forEach_** to console.log each name in the names array.
+
+names.forEach((element) => console.log(element));
+
+//Exercise 5
+// Use **_forEach_** to console.log each number in the numbers array.
+
+numbers.forEach((element) => console.log(element));
+
+
