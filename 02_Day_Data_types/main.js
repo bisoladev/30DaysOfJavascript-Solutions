7c66c560b784d9a74e954f6634a3edd8c17f9ca2
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-01-28 15:36:58 +0100

    Modified Level 3, Question 1

--- 02_Day_Data_types/main.js
+++ 02_Day_Data_types/main.js
@@ -164,21 +164,28 @@ console.log(cutOut);
 
 //Level 3
 
-//Question 1
-let loveStatement =
-  "Love is the best thing in this world. Some found their love and some \
-are still looking for their love.";
+// Question 1
+// 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word __love__ in this sentence.
+
+let loveStatement = "Love is the best thing in this world. Some found their love and some \ are still looking for their love.";
 
 let lovePattern = /love/gi;
 let loveCount = loveStatement.match(lovePattern).length;
 console.log(loveCount);
 
-//Question 2
+// Question 2
+// Use __match()__ to count the number of all __because__ in the following sentence:__'You cannot end a sentence with because because because is a conjunction'__
+
 let becausePattern = /because/gi;
 let becauseCount = sentence.match(becausePattern).length;
 console.log(becauseCount);
 
 //Question 3
+// Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+
+//    ```js
+//        const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+//    ```
 const badSentence =
   "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;.\
  The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. \
@@ -233,10 +240,11 @@ function findMostRepeatedWord(replacedSymbols) {
   return mostRepeatedWord;
 }
 
-//Question 4
-const income =
-  "He earns 5000 euro from salary per month, \
-10000 euro annual bonus, 15000 euro online courses per month.";
+// Question 4
+// Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+// const income =
+//   "He earns 5000 euro from salary per month, \
+// 10000 euro annual bonus, 15000 euro online courses per month.";
 
 const digitPattern = /\d+/g;
 let digitWords = income.match(digitPattern);
